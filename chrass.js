0b59c9d9f24ca22b775fc72600637d7206e917f9
Carlos Quiroz <cquiroz@ketalo.com>
2012-07-02 22:24:39 -0400

    Fix for the case of a css rule containing quotes. The quotes are removed on the styles list and on the matching part too. Added better error handling

--- chrass.js
+++ chrass.js
@@ -68,32 +68,35 @@ function findSassResources(cssResources) {
  * the file
  */
 var pageFindSassInfo = function(element) {
-    // $0 contains the element selected
-    var data = $0;
-    var sassValues = JSON.parse(element);
-    // Make a shallow copy with a null prototype, so that sidebar does not
-    // expose prototype.
-    var props = Object.getOwnPropertyNames(data);
-    var copy = {
-        __proto__: null
-    };
-    // Extract the matched css rules
-    var rules = window.getMatchedCSSRules(data, '')
-    for (var j = 0;j<rules.length;j++) {
-        // If some of the rules matches a sass debug info add it to the content
-        // of the sidebar
-        if (typeof sassValues[rules[j].selectorText] != 'undefined') {
-            var className = rules[j].selectorText;
-            // use only the file name, not the full path
-            var fileName = sassValues[className]['filename'].replace(/^.*[\\\/]/, ''); 
-            var linenum = sassValues[className]['linenum'];
-            // Get the debug info in the form of filename:linenum
-            var sassDebug =  fileName + ':' + linenum;
-            copy[className] = sassDebug;
+    try {
+        // $0 contains the element selected
+        var data = $0;
+        var sassValues = JSON.parse(element);
+        var copy = {
+            __proto__: null
+        };
+        // Extract the matched css rules
+        var rules = window.getMatchedCSSRules(data, '');
+        if (rules) {
+            for (var j = 0;j<rules.length;j++) {
+                // If some of the rules matches a sass debug info add it to the content
+                // of the sidebar
+                if (typeof sassValues[rules[j].selectorText] !== 'undefined') {
+                    var className = rules[j].selectorText;
+                    // use only the file name, not the full path
+                    var fileName = sassValues[className]['filename'].replace(/^.*[\\\/]/, '');
+                    var linenum = sassValues[className]['linenum'];
+                    // Get the debug info in the form of filename:linenum
+                    var sassDebug =  fileName + ':' + linenum;
+                    copy[className] = sassDebug;
+                }
+            }
         }
+        return copy;
+    } catch(err) {
+       return {'error': "Error description: " + err.message };
     }
-    return copy;
-}
+};
 
 /**
  * Creates the sidebar
@@ -103,12 +106,16 @@ function buildSidebar(pageContents) {
         // on version 0.2 we display a page containing all found sass styles
         // sass has been found
         function updateElementProperties() {
-            // The expression is a function text where we pass the parameter
-            // and it is executed in the context of the elements panel
-            // The page contents need to be passed as a string and re-parsed
-            // inside
-            var expression = "(" + pageFindSassInfo.toString() + ")('" + JSON.stringify(pageContents) + "');";
-            sidebar.setExpression(expression);
+            try {
+                // The expression is a function text where we pass the parameter
+                // and it is executed in the context of the elements panel
+                // The page contents need to be passed as a string and re-parsed
+                // inside
+                var expression = "(" + pageFindSassInfo.toString() + ")('" + JSON.stringify(pageContents) + "');";
+                sidebar.setExpression(expression);
+            } catch (error) {
+                sidebar.setObject({error: error.message});
+            }
         }
         chrome.devtools.panels.elements.onSelectionChanged.addListener(updateElementProperties);
     });
